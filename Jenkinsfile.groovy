c3c411046c347bf0c950bce4efb26717ff680e72
pavel-lucik <52709415+pavel-lucik@users.noreply.github.com>
2019-11-19 19:25:51 +0200

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,32 @@
+properties([
+pipelineTriggers([])
+])
+
+node () {
+    
+    stage ("average") {
+        sh 'uptime'
+        deleteDir()
+        
+    }
+    
+    stage ("git") {
+        withCredentials([usernamePassword(credentialsId: 'd1188926-b2e6-41b1-a7fa-1adc494fa6fc', passwordVariable: 'password', usernameVariable: 'username')]) {
+        sh 'git clone https://$username:$password@github.com/pavel-lucik/gw-spring-boot.git'}
+    }    
+
+    stage ("pwd and build"){
+        def workDir = sh(returnStdout: true, script: "pwd").trim()
+        sh "cd $workDir && cd gw-spring-boot/spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-ui && ls -lh && mvn clean install"
+    }
+}
+
+input("Please approve deploy to...")
+
+node(){
+    stage ("upload artifact"){
+        def workDir = sh(returnStdout: true, script: "pwd").trim()
+        sh "cd $workDir && cd gw-spring-boot/spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-ui && ls -lh"
+        archiveArtifacts artifacts: "**/target/*.jar", fingerprint: true
+    }
+}
